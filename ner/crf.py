b123cfabbc30a53d51b8725ba7366bcd5ae42f6b
HustElvan <1099270667@qq.com>
2019-09-10 14:24:30 +0800

    add file

---
+++ ner/crf.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+# @Author: huzhu
+# @Date:   2019-09-09 22:04:31
+# @Last Modified by:   huzhu
+# @Last Modified time: 2019-09-10 14:23:36
+import nltk
+from itertools import chain
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelBinarizer
+import pycrfsuite
+# nltk.download('conll2002')
+
+model_path = 'model/crf.model'
+train_sents = list(nltk.corpus.conll2002.iob_sents('esp.train'))
+test_sents = list(nltk.corpus.conll2002.iob_sents('esp.testb'))
+
+# print(train_sents[0:3])
+# print(test_sents[0:3])
+
+def word2features(sent, i):
+    word = sent[i][0]
+    postag = sent[i][1]
+    features = [
+        'bias',
+        'word.lower=' + word.lower(),
+        'word[-3:]=' + word[-3:],
+        'word[-2:]=' + word[-2:],
+        'word.isupper=%s' % word.isupper(),
+        'word.istitle=%s' % word.istitle(),
+        'word.isdigit=%s' % word.isdigit(),
+        'postag=' + postag,
+        'postag[:2]=' + postag[:2],
+    ]
+    if i > 0:
+        word1 = sent[i - 1][0]
+        postag1 = sent[i - 1][1]
+        features.extend([
+            '-1:word.lower=' + word1.lower(),
+            '-1:word.istitle=%s' % word1.istitle(),
+            '-1:word.isupper=%s' % word1.isupper(),
+            '-1:postag=' + postag1,
+            '-1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('BOS')
+    if i < len(sent) - 1:
+        word1 = sent[i + 1][0]
+        postag1 = sent[i + 1][1]
+        features.extend([
+            '+1:word.lower=' + word1.lower(),
+            '+1:word.istitle=%s' % word1.istitle(),
+            '+1:word.isupper=%s' % word1.isupper(),
+            '+1:postag=' + postag1,
+            '+1:postag[:2]=' + postag1[:2],
+        ])
+    else:
+        features.append('EOS')
+    return features
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+def sent2labels(sent):
+    return [label for token, postag, label in sent]
+
+def sent2tokens(sent):
+    return [token for token, postag, label in sent]
+
+def train():
+    print(sent2features(train_sents[0])[0])
+    X_train = [sent2features(s) for s in train_sents]
+    y_train = [sent2labels(s) for s in train_sents]
+
+    trainer = pycrfsuite.Trainer(verbose=False)
+    trainer.set_params({
+        'c1': 1.0,  # coefficient for L1 penalty
+        'c2': 1e-3,  # coefficient for L2 penalty
+        'max_iterations': 50,  # stop earlier
+        # include transitions that are possible, but not observed
+        'feature.possible_transitions': True
+    })
+
+    print(trainer.params())
+
+    for xseq, yseq in zip(X_train, y_train):
+        trainer.append(xseq, yseq)
+
+    trainer.train(model_path)
+    print(trainer.logparser.last_iteration)
+
+def predict():
+    tagger = pycrfsuite.Tagger()
+    tagger.open(model_path)
+    example_sent = test_sents[3]
+    print(' '.join(sent2tokens(example_sent)), end='\n\n')
+    print("Predicted:", ' '.join(tagger.tag(sent2features(example_sent))))
+    print("Correct:  ", ' '.join(sent2labels(example_sent)))
+
+
+def bio_classification_report(y_true, y_pred):
+    lb = LabelBinarizer()
+    y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
+    y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
+
+    tagset = set(lb.classes_) - {'O'}
+    tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
+    class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
+
+    return classification_report(
+        y_true_combined,
+        y_pred_combined,
+        labels=[class_indices[cls] for cls in tagset],
+        target_names=tagset,
+    )
+
+
+def evaluate():
+    tagger = pycrfsuite.Tagger()
+    tagger.open(model_path)
+    X_test = [sent2features(s) for s in test_sents]
+    y_test = [sent2labels(s) for s in test_sents]
+    y_pred = [tagger.tag(xseq) for xseq in X_test]
+    print(bio_classification_report(y_test, y_pred))
+
+
+if __name__ == '__main__':
+    #train()
+    predict()
+    #evaluate()
