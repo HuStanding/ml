e46ebc1b08121eab459f8683d7c2d0525f0babe4
HustElvan <1099270667@qq.com>
2019-09-25 21:56:50 +0800

    update

--- machine_learning/bayes/bayes.py
+++ machine_learning/bayes/bayes.py
@@ -2,9 +2,11 @@
 # @Author: huzhu
 # @Date:   2019-09-19 20:43:50
 # @Last Modified by:   huzhu
-# @Last Modified time: 2019-09-24 21:11:40
+# @Last Modified time: 2019-09-25 21:55:37
 # @description p(ci|w) = p(w|ci)p(ci)/p(w)
 
+import re
+import codecs
 from numpy import *
 
 def load_dataSet():
@@ -33,7 +35,7 @@ def create_vocablist(dataSet):
 	return list(vocabSet)
 
 
-def set_of_words2vec(vocablist, inputset):
+def bag_of_words2vec(vocablist, inputset):
 	"""
 	@brief      Sets the of words 2 vector.
 	@param      vocablist  The vocablist
@@ -43,9 +45,7 @@ def set_of_words2vec(vocablist, inputset):
 	res = [0] * len(vocablist)
 	for word in inputset:
 		if word in vocablist:
-			res[vocablist.index(word)] = 1
-		else:
-			print("the word: %s is not in my vocabulary!") % word 
+			res[vocablist.index(word)] += 1
 	return res 
 
 def train(train_matrix, train_category):
@@ -94,14 +94,68 @@ def test():
 	my_vocablist = create_vocablist(list_posts)
 	train_matrix = []
 	for doc in list_posts:
-		train_matrix.append(set_of_words2vec(my_vocablist, doc))
+		train_matrix.append(bag_of_words2vec(my_vocablist, doc))
 	p0,p1,pAb = train(train_matrix, list_class)
 	test_entry = ["stupid", "garbage", "dalmation"]
-	this_doc = set_of_words2vec(my_vocablist,test_entry)
+	this_doc = bag_of_words2vec(my_vocablist,test_entry)
 	print("test_entry is class ", classify(this_doc, p0, p1, pAb))
 
+def text_parse(big_string):
+	"""
+	@brief      长字符串解析过程
+	@param      big_string  The big string
+	@return     处理后的字符串列表
+	"""
+	list_of_tokens = re.split(r"\W*", big_string)
+	return [token.lower() for token in list_of_tokens if len(token) > 2]
+
+def spam_test():
+	"""
+	@brief      利用贝叶斯分类器自动化处理
+	@return     无
+	"""
+	doc_list = []
+	class_list = []
+	full_text = []
+	for i in range(1,26):
+		# 循环读取文件夹下的邮件内容
+		word_list = text_parse(codecs.open("../machine_learning_inaction_sourcecode/Ch04/email/spam/%d.txt" % i, 
+								"r", encoding = "utf-8", errors='ignore').read())
+		doc_list.append(word_list)
+		full_text.extend(word_list)
+		class_list.append(1)
+		word_list = text_parse(codecs.open("../machine_learning_inaction_sourcecode/Ch04/email/ham/%d.txt" % i,
+								"r", encoding = "utf-8", errors='ignore').read())
+		doc_list.append(word_list)
+		full_text.extend(word_list)
+		class_list.append(0)
+	vocablist = create_vocablist(doc_list)
+
+	# 从中随机构建训练数据和测试数据
+	trainSet = list(range(50))
+	testSet = []
+	for i in range(10):
+		index = int(random.uniform(0,len(trainSet)))
+		testSet.append(index)
+		del(trainSet[index])
+	train_matrix = []
+	train_category = []
+	for index in trainSet:
+		train_matrix.append(bag_of_words2vec(vocablist, doc_list[index]))
+		train_category.append(class_list[index])
+	# 开始训练
+	p0,p1,pAb = train(train_matrix, train_category)
+	# 统计识别率
+	error_count = 0
+	for doc_index in testSet:
+		word_vector = bag_of_words2vec(vocablist, doc_list[doc_index])
+		if classify(word_vector, p0,p1,pAb) != class_list[doc_index]:
+			error_count += 1
+	print("the error rate is :", float(error_count) / len(testSet))
 
 if __name__ == '__main__':
 	test()
+	spam_test()
+
 
 
